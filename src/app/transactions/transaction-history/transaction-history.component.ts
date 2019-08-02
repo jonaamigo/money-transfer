f0f8ee864ffd7e4bd60fbccec2872e3ef72c4014
jonaamigo <jonathan.amigo.torres@gmail.com>
2019-08-02 10:57:39 +0100

    transaction-history orderby and filters

--- src/app/transactions/transaction-history/transaction-history.component.ts
+++ src/app/transactions/transaction-history/transaction-history.component.ts
@@ -1,7 +1,7 @@
+import { Transaction } from './../../shared/model/transaction';
 import { TransactionsService } from './../transactions.service';
 import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { Transaction } from 'src/app/shared/model/transaction';
 
 @Component({
   selector: 'app-transaction-history',
@@ -11,15 +11,11 @@ import { Transaction } from 'src/app/shared/model/transaction';
 export class TransactionHistoryComponent implements OnInit {
 
   myControl = new FormControl();
-  options: any[] = [
-    { name: 'Mary' },
-    { name: 'Shelley' },
-    { name: 'Igor' }
-  ];
   color: string;
   transactions: Array<Transaction> = [];
   orderBy = 'DATE';
   orderByDesc = false;
+  filter: string;
 
   constructor(private transactionsService: TransactionsService) { }
 
@@ -32,5 +28,39 @@ export class TransactionHistoryComponent implements OnInit {
       this.orderByDesc = !this.orderByDesc;
     }
     this.orderBy = orderBy;
+    this.transactions = this.getTransactions();
+  }
+
+  getTransactions() {
+    let transactions = this.transactionsService.getTransactions().map(x => ({ ...x }));
+    if (!!this.filter) {
+      transactions = transactions.filter(x => x.beneficiary.toLowerCase().includes(this.filter.toLowerCase()));
+    }
+    transactions.sort((e1, e2) => this.sortFunction(e1, e2, this.orderBy));
+    return transactions;
+  }
+
+  sortFunction(e1: Transaction, e2: Transaction, orderBy: string) {
+    let comp = 0;
+    switch (orderBy) {
+      case 'DATE':
+        comp = e1.date.getTime() - e2.date.getTime();
+        break;
+      case 'BENEFICIARY':
+        comp = ('' + e1.beneficiary).localeCompare(e2.beneficiary);
+        break;
+      case 'AMOUNT':
+        comp = e1.amount - e2.amount;
+        break;
+    }
+
+    if (this.orderByDesc) {
+      return comp * -1;
+    }
+    return comp;
+  }
+
+  onChangeFilter() {
+    this.transactions = this.getTransactions();
   }
 }
