5995458fe3a73b423d1eafa993676a3b340dffd1
jonaamigo <jonathan.amigo.torres@gmail.com>
2019-08-02 11:48:47 +0100

    transfer form

--- src/app/transactions/transfer-form/transfer-form.component.ts
+++ src/app/transactions/transfer-form/transfer-form.component.ts
@@ -1,5 +1,7 @@
+import { TransactionsService } from './../transactions.service';
 import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Transfer } from 'src/app/shared/model/transfer';
 
 @Component({
   selector: 'app-transfer-form',
@@ -8,16 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransferFormComponent implements OnInit {
 
-  myControl = new FormControl();
-  options: any[] = [
-    {name: 'Mary'},
-    {name: 'Shelley'},
-    {name: 'Igor'}
+  accountsFrom: any[] = [
+    { text: 'Account 0', value: 0 },
+    { text: 'Account 1', value: 1 },
+    { text: 'Account 2', value: 2 }
   ];
-    color: string;
-  constructor() { }
+  accountsTo: any[] = [
+    { text: 'Account to 0', value: 0 },
+    { text: 'Account to 1', value: 1 },
+    { text: 'Account to 2', value: 2 }
+  ];
+  accountFrom: number;
+  accountTo: number;
+  amount: string;
+
+  constructor(private transactionsService: TransactionsService) { }
 
   ngOnInit() {
   }
 
+  onClickSubmit() {
+    const transfer = new Transfer();
+    transfer.accountFrom = this.accountsFrom[this.accountFrom].text;
+    transfer.accountTo = this.accountsTo[this.accountTo].text;
+    transfer.amount = Number(this.amount);
+    transfer.date = new Date();
+    this.transactionsService.addTransfer(transfer);
+  }
 }
